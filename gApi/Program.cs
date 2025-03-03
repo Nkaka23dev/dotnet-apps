be1cb9ffbaf54050cd1ac1cee8f044625c7c5f95
ericnkaka <nkakaeric96@gmail.com>
2025-03-03 17:03:51 +0200

    feat: JSON API Route

--- gApi/Program.cs
+++ gApi/Program.cs
@@ -1,5 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 
+var builder = WebApplication.CreateBuilder(args);
+var employees = new List<Employee> {
+    new Employee {Id = 1, FirstName= "Eric", LastName="Nkaka"},
+    new Employee {Id = 2, FirstName= "Joe", LastName="Doe"}
+};
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -12,8 +18,10 @@ builder.Services.AddOpenApiDocument(config =>
 });
 var app = builder.Build();
 
+var employeeRoute = app.MapGroup("employees");
+
 if (app.Environment.IsDevelopment())
-{
+{   
     app.UseOpenApi();
     app.UseSwaggerUi(config =>
     {
@@ -24,36 +32,25 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+employeeRoute.MapGet(string.Empty, () => {
+   return Results.Ok(employees);
+});
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+employeeRoute.MapGet("{id:int}", ([FromRoute] int id) => {
+    var employee = employees.SingleOrDefault(e => e.Id == id);
+    if(employee == null){
+        return Results.NotFound();
+    }
+    return Results.Ok(employee);
+});
+
+employeeRoute.MapPost(string.Empty, ([FromBody]Employee employee, HttpContext context) => {
+    employee.Id = employees.Max(e => e.Id) + 1; 
+    employees.Add(employee);
+    return Results.Created($"employees/{employee.Id}", employee);
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- gApi/obj/Debug/net9.0/gApi.AssemblyInfo.cs
+++ gApi/obj/Debug/net9.0/gApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6977d9f46e9412d5880434948d49b9611c25088")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a5537ec6bb4774be90718518fe1a00481526a15")]
 [assembly: System.Reflection.AssemblyProductAttribute("gApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
